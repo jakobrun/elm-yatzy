218ed510a89e75f27e08290b51b9919671091e91
jakob <jakob.runarsson@gmail.com>
2016-08-24 22:31:35 +0000

    fix basscss

--- server/index.js
+++ server/index.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const express = require('express'),
-  path = require('path'),
   port = process.env.PORT || 3030,
   app = express();
 
 app.use(express.static('build'));
 app.use(express.static('web'));
-const basePath = path.join(__dirname, '../node_modules/');
-app.use('/basscss', express.static(path.join(basePath, 'basscss/css')));
+app.use('/basscss', express.static('node_modules/basscss/css'));
 
 app.listen(port, function() {
   console.log('server listening on port:', port);
